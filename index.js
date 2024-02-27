a277b59646c55f6e13ded3273adbbe817301a1f2
udhaykumarbala <udhayk97@gmail.com>
2024-02-27 08:41:58 +0530

    initial

---
+++ index.js
@@ -0,0 +1,50 @@
+const ccxt = require ('ccxt').pro;
+const { createClient } = require('redis');
+
+(async () => {
+
+  const client = createClient({
+    host: 'localhost',
+    port: 6379,
+    password: 'password'
+  })
+  await client.connect()
+  const redisBaseKey = 'price:'
+
+
+  const exchange = new ccxt.bybit({ enableRateLimit: true })
+  while (true) {
+    const tickers = await exchange.watchTickers([ 'BTC/USDT', 'ETH/USDT', 'SOL/USDT', 'DOGE/USDT', 'MATIC/USDT','LTC/USDT', 'LINK/USDT','TRX/USDT','RUNE/USDT','AAVE/USDT'])
+    // const tickers = await exchange.watchTickers(['USDT/EUR', 'PAXG/USDT'])
+    const stableCoins = {}
+    const timestamp = new Date().getTime()
+    const expiryTimestamp = timestamp + 15 // 15 seconds
+    const tickersData = { ...tickers, ...stableCoins }
+    // console.log('tickersData', tickersData)
+    for (let symbol in tickersData) {
+      let price = tickersData[symbol].info.lastPrice
+      
+      if (symbol === 'USDT/EUR') {
+        price = (1 / price).toFixed (4)
+        symbol = 'EUR/USDT'
+      }
+
+      symbol =  symbol.replace ('/', ':')
+      const key = redisBaseKey + symbol
+      client.hSet(key,{
+        Price: price,
+        CreatedAt: timestamp,
+        ExpireAt: expiryTimestamp
+      })
+
+      // publish to redis on price change topic
+      
+      
+      // console.log (timestamp, expiryTimestamp, symbol, price)
+      client.publish('price-change', 'price changed')
+    }
+
+
+  }
+
+}) ()
