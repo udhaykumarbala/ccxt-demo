ecd0b6c96a5d7d7512bd6013f4599afdce003111
udhaykumarbala <udhayk97@gmail.com>
2024-10-01 12:53:15 +0530

    update

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const { createClient } = require('redis');
 
 
   const exchange = new ccxt.bybit({ enableRateLimit: true })
+  let allAssets = [ 'BTC/USDT', 'ETH/USDT', 'SOL/USDT', 'DOGE/USDT', 'LTC/USDT', 'LINK/USDT','TRX/USDT','RUNE/USDT','AAVE/USDT'];
+  let secondaryCheckAssets = [];
   while (true) {
     let tickers = {}
-    let allAssets = [ 'BTC/USDT', 'ETH/USDT', 'SOL/USDT', 'DOGE/USDT', 'LTC/USDT', 'LINK/USDT','TRX/USDT','RUNE/USDT','AAVE/USDT'];
     // tickers = await exchange.watchTickers(['USDT/EUR', 'PAXG/USDT']).catch((e) => {
     tickers = await exchange.watchTickers(allAssets).catch((e) => {
+      // add error causing asset to secondaryCheckAssets
+      asset = e.message.split(' ').pop()
+      allAssets = allAssets.filter(a => a !== asset)
+      secondaryCheckAssets.push(asset)
       let timeNowIndia = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
       console.log(timeNowIndia,'Error in watchTickers', e)
     })
@@ -52,48 +57,46 @@ const { createClient } = require('redis');
       // console.log (timeNowIndia, expiryTimestamp, symbol, price)
     }
 
-    // get MATIC/USDT price from binance
-    const binance = new ccxt.binance({ enableRateLimit: true })
-    const maticPrice = await binance.fetchTicker('MATIC/USDT').catch((e) => {
+    // check if secondaryCheckAssets has any assets 
+    if (secondaryCheckAssets.length > 0) {
+      // get price from binance
+      const binance = new ccxt.binance({ enableRateLimit: true })
+      const secondaryCheckAssetsPrice = await binance.fetchTicker(secondaryCheckAssets).catch((e) => {
+      
       let timeNowIndia = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
       console.log(timeNowIndia,'Error in fetchTicker', e)
     })
 
-    // filter not price fetched assets from allAssets
-    allAssets = allAssets.filter(asset => asset !== 'MATIC/USDT')
-
-    const maticPriceKey = redisBaseKey + 'MATIC:USDT'
-    const price = maticPrice.last
-    const priceChangePercent24h = maticPrice.percentage
-    client.hSet(maticPriceKey,{
-      Price: price,
-      CreatedAt: timestamp,
-      ExpireAt: expiryTimestamp,
-      PriceChangePercent24h: priceChangePercent24h
-    })
-
-
-
+    // set price in redis
+    for (let symbol in secondaryCheckAssetsPrice) {
+      let price = secondaryCheckAssetsPrice[symbol].last
+      let priceChangePercent24h = secondaryCheckAssetsPrice[symbol].percentage
+      symbol = symbol.replace ('/', ':')
+      const key = redisBaseKey + symbol
+      client.hSet(key,{
+        Price: price,
+        CreatedAt: timestamp,
+        ExpireAt: expiryTimestamp,
+        PriceChangePercent24h: priceChangePercent24h
+      })
+    }
 
 
-
-
-    const maticPriceKey = redisBaseKey + 'MATIC:USDT'
-    const price = 0.3794
-    const priceChangePercent24h = 0
-    client.hSet(maticPriceKey,{
-      Price: price,
-      CreatedAt: timestamp,
-      ExpireAt: expiryTimestamp,
-      PriceChangePercent24h: priceChangePercent24h
-    })
+   
 
     timeNowIndia = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
 
     // console.log ("binance", timeNowIndia, expiryTimestamp, 'MATIC:USDT', price)
-
-    client.publish('price-change', 'price changed')
     
   }
 
-}) ()
+  client.hSet('price:MATIC/USDT',{
+    Price: 0.4042,
+    CreatedAt: timestamp,
+    ExpireAt: expiryTimestamp,
+    PriceChangePercent24h: 0
+  })
+
+  client.publish('price-change', 'price changed')
+  }
+})()
